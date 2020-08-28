94c35c30c0cd491a57bd98ccb27a4cd42e74bee6
unknown <chanhuicom@gmail.com>
2020-08-28 21:39:05 +0900

    2.0 : Local State part One - Create Like Buttons

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -3,25 +3,30 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-  height: 380px;
+  height: 400px;
+  border-radius: 7px;
   width: 100%;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
-  border-radius: 7px;
+  border-radius: 7px;.
+  background-color: transparent;
 `;
 
 const Poster = styled.div`
   background-image: url(${props => props.bg});
   height: 100%;
   width: 100%;
+  border-radius: 7px;
   background-size: cover;
   background-position: center center;
 `;
 
-export default ({ id, bg }) => (
+export default ({ id, bg, isLiked }) => (
+  <>
   <Container>
     <Link to={`/${id}`}>
       <Poster bg={bg} />
+      <button>{isLiked ? "Unlike" : "Like"}</button>
     </Link>
   </Container>
+  </>
 );
